fdfad0b93b782bdd4a2ad610cbe8fc0454a31f36
Abhijit Shetty <abhijeet.shetty1995@gmail.com>
2026-05-05 22:49:40 -0400

    Update encoders __all__ entry for ConvNext adapter
    Renames the ConvNextDummyEncoder reference in __all__ to
    ConvNextEncoderAdapter, matching the naming the real adapter will use
    when it lands.

--- src/aac_baseline/models/encoders/__init__.py
+++ src/aac_baseline/models/encoders/__init__.py
@@ -2,4 +2,4 @@ from .ast_adapter import ASTEncoderAdapter
 from .beats_adapter import BeatsEncoderAdapter
 from .convnext_dummy import ConvNextDummyEncoder
 
-__all__ = ["ASTEncoderAdapter", "BeatsEncoderAdapter", "ConvNextDummyEncoder"]
+__all__ = ["ASTEncoderAdapter", "BeatsEncoderAdapter", "ConvNextEncoderAdapter"]
